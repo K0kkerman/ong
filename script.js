3ad05e3207dec4eae1886ac02a6e979646db545e
Roihuvuori <121727788+Roihuvuori@users.noreply.github.com>
2023-05-15 02:19:11 +0300

    fat

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
 var backgroundVideo = document.getElementById('background-video');
 
-function handleVisibilityChange() {
-  if (document.hidden) {
-    backgroundVideo.pause();
-  } else {
-    backgroundVideo.play();
-  }
-}
-
-document.addEventListener('visibilitychange', handleVisibilityChange);
+backgroundVideo.pause();
